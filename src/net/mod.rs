07e826844d6850bec0aedd9a67cf6ec880f3fc60
str4d <str4d@mail.i2p>
2017-04-30 18:48:51 +1200

    Internally split out I2pStream into streaming submodule

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,19 +1,13 @@
-use std::io::prelude::*;
-
-use std::fmt;
 use std::io;
-use std::net::{Shutdown, SocketAddr, ToSocketAddrs};
-
-use rand;
-use rand::Rng;
-
-use sam::Stream;
+use std::net::{SocketAddr, ToSocketAddrs};
 
 pub use self::addr::{I2pSocketAddr, ToI2pSocketAddrs};
 pub use self::i2p::I2pAddr;
+pub use self::streaming::I2pStream;
 
 mod addr;
 mod i2p;
+mod streaming;
 #[cfg(test)]
 mod test;
 
@@ -40,74 +34,4 @@ where
             },
         ),
     )
-}
-
-pub struct I2pStream {
-    inner: Stream,
-}
-
-impl I2pStream {
-    pub fn connect<A: ToI2pSocketAddrs>(addr: A) -> io::Result<I2pStream> {
-        I2pStream::connect_via("127.0.0.1:7656", addr)
-    }
-
-    pub fn connect_via<A: ToSocketAddrs, B: ToI2pSocketAddrs>(sam_addr: A, addr: B) -> io::Result<I2pStream> {
-        each_addr(sam_addr, addr, I2pStream::connect_addr)
-    }
-
-    fn connect_addr(sam_addr: &SocketAddr, addr: &I2pSocketAddr) -> io::Result<I2pStream> {
-        let suffix: String = rand::thread_rng().gen_ascii_chars().take(8).collect();
-        let nickname = format!("i2prs-{}", suffix);
-
-        let stream = Stream::new(sam_addr, &addr.dest().string(), addr.port(), &nickname)?;
-
-        Ok(I2pStream { inner: stream })
-    }
-
-    pub fn peer_addr(&self) -> io::Result<I2pAddr> {
-        self.inner.peer_addr().map(|a| I2pAddr::new(&a))
-    }
-
-    pub fn local_addr(&self) -> io::Result<I2pAddr> {
-        self.inner.local_addr().map(|a| I2pAddr::new(&a))
-    }
-
-    pub fn shutdown(&self, how: Shutdown) -> io::Result<()> {
-        self.inner.shutdown(how)
-    }
-
-    pub fn try_clone(&self) -> io::Result<I2pStream> {
-        self.inner.duplicate().map(|s| I2pStream { inner: s })
-    }
-}
-
-impl Read for I2pStream {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.inner.read(buf)
-    }
-}
-
-impl Write for I2pStream {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.inner.write(buf)
-    }
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-impl fmt::Debug for I2pStream {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut res = f.debug_struct("I2pStream");
-
-        if let Ok(addr) = self.local_addr() {
-            res.field("addr", &addr);
-        }
-
-        if let Ok(peer) = self.peer_addr() {
-            res.field("peer", &peer);
-        }
-
-        res.finish()
-    }
 }
